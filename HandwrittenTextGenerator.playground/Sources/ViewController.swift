6f8c86c86ca15c979ca28d9a5beb88da59bf0f15
LeoAOliveira <leonardo.aoliveira@icloud.com>
2020-11-02 02:32:03 -0300

    Creating textImageView
    Creating a textImageView, setting it’s image as text.png and adding to the collageView

--- HandwrittenTextGenerator.playground/Sources/ViewController.swift
+++ HandwrittenTextGenerator.playground/Sources/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 import PlaygroundSupport
 
+// MARK: - ViewController
+
 public class ViewController: UIViewController {
     
     // MARK: - Properties
@@ -19,13 +21,25 @@ public class ViewController: UIViewController {
         return view
     }()
     
+    private lazy var textImageView: UIImageView = {
+        let imageView: UIImageView = UIImageView()
+        imageView.backgroundColor = .white
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
     }()
     
     private lazy var constraints: [NSLayoutConstraint] = [
+        self.collageView.widthAnchor.constraint(equalToConstant: self.view.frame.width),
+        self.collageView.heightAnchor.constraint(equalToConstant: self.view.frame.width),
         self.collageView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
         self.collageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
     ]
     
+    private lazy var imagesConstraints: [NSLayoutConstraint] = [
+        self.textImageView.topAnchor.constraint(equalTo: self.collageView.topAnchor, constant: 10),
+        self.textImageView.leadingAnchor.constraint(equalTo: self.collageView.leadingAnchor, constant: 10),
+        self.textImageView.trailingAnchor.constraint(equalTo: self.collageView.trailingAnchor, constant: -10),
+        self.textImageView.bottomAnchor.constraint(equalTo: self.collageView.bottomAnchor, constant: -10)
     ]
     
     // MARK: - Override functions
@@ -34,18 +48,39 @@ public class ViewController: UIViewController {
         view.backgroundColor = .lightGray
         view.addSubview(collageView)
         
+        collageView.addSubview(textImageView)
+        
+        guard let image = UIImage(named: "text.png") else {
+            return
+        }
+        
+        textImageView.image = image
+        TextRecognition().boxRequest(image: image, imageView: textImageView)
+        
         imageGenerator()
     }
     
     public override func viewDidLayoutSubviews() {
         NSLayoutConstraint.activate(constraints)
+        NSLayoutConstraint.activate(imagesConstraints)
     }
     
     // MARK: - Functions
     
     private func imageGenerator() {
         
+//        UIGraphicsBeginImageContext(collageView.frame.size)
+//        
+//        collageView.layer.render(in: UIGraphicsGetCurrentContext()!)
+//        
+//        if let image = UIGraphicsGetImageFromCurrentImageContext() {
+//            // saveImage(image)
+//        }
+    }
+    
+    private func saveImage(_ image: UIImage) {
         
+        if let data = image.pngData() {
             
             let filename = playgroundSharedDataDirectory.appendingPathComponent("generatedImage.png")
         
