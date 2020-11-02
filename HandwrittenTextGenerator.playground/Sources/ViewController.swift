012a439b9ee9307c1cedf8a1779a9f53ad14e292
LeoAOliveira <leonardo.aoliveira@icloud.com>
2020-11-02 02:51:01 -0300

    Drawing boxes in recognized words and character s
    Drawing boxes in recognized words and character for a given image.

--- HandwrittenTextGenerator.playground/Sources/ViewController.swift
+++ HandwrittenTextGenerator.playground/Sources/ViewController.swift
@@ -132,8 +132,10 @@ public class TextRecognition {
                 }
                 
                 for currentObservation in observations {
+                    self.highlightWord(box: currentObservation)
                     
                     for currentCharacterBox in currentObservation.characterBoxes! {
+                        self.highlightLetters(box: currentCharacterBox)
                     }
                 }
                 
@@ -149,4 +151,55 @@ public class TextRecognition {
         }
     }
     
+    private func highlightWord(box: VNTextObservation) {
+        guard let boxes = box.characterBoxes else {
+            return
+        }
+            
+        var maxX: CGFloat = 9999.0
+        var minX: CGFloat = 0.0
+        var maxY: CGFloat = 9999.0
+        var minY: CGFloat = 0.0
+            
+        for char in boxes {
+            if char.bottomLeft.x < maxX {
+                maxX = char.bottomLeft.x
+            }
+            if char.bottomRight.x > minX {
+                minX = char.bottomRight.x
+            }
+            if char.bottomRight.y < maxY {
+                maxY = char.bottomRight.y
+            }
+            if char.topRight.y > minY {
+                minY = char.topRight.y
+            }
+        }
+            
+        let xCord = maxX * imageView.frame.size.width
+        let yCord = (1 - minY) * imageView.frame.size.height
+        let width = (minX - maxX) * imageView.frame.size.width
+        let height = (minY - maxY) * imageView.frame.size.height
+            
+        let outline = CALayer()
+        outline.frame = CGRect(x: xCord, y: yCord, width: width, height: height)
+        outline.borderWidth = 2.0
+        outline.borderColor = UIColor.red.cgColor
+            
+        imageView.layer.addSublayer(outline)
+    }
+    
+    private func highlightLetters(box: VNRectangleObservation) {
+        let xCord = box.topLeft.x * imageView.frame.size.width
+        let yCord = (1 - box.topLeft.y) * imageView.frame.size.height
+        let width = (box.topRight.x - box.bottomLeft.x) * imageView.frame.size.width
+        let height = (box.topLeft.y - box.bottomLeft.y) * imageView.frame.size.height
+            
+        let outline = CALayer()
+        outline.frame = CGRect(x: xCord, y: yCord, width: width, height: height)
+        outline.borderWidth = 1.25
+        outline.borderColor = UIColor.blue.cgColor
+        
+        imageView.layer.addSublayer(outline)
+    }
 }
